ea3156417bc8826f11c5a1a0292d32d1eb33b625
Adrien <adrien.pouxviel@gmail.com>
2022-10-17 17:54:56 +0200

    fix: dropdown menu divider

--- metadot_navbar.js
+++ metadot_navbar.js
@@ -1,19 +1,12 @@
-console.log("coucou");
 metadot_navbar.innerHTML = `
-
 <style>
 
-.brand-logo{
-    width:85px;
-    height:15px;
-}
-.nav-link{
+.metadot-link{
     cursor:pointer;
-    color:white;
 }
 
 .product-title{
-    font-size:18px;
+    font-size:14px;
 }
 
 .product-item img{
@@ -29,11 +22,12 @@ metadot_navbar.innerHTML = `
     border: 0;
     display:flex;
     max-width: 300px;
-    min-height:141px;
 }
 
 .product-item p{
-font-size:13px;
+    font-size:12px;
+    color: #4d4d4d;
+    line-height: 13px;
 }
 
 .product-item:focus, .product-item:hover {
@@ -42,124 +36,100 @@ font-size:13px;
     background-color: #e9ecef;
 }
 
+.solutions-item{
+    font-size:14px;
+    color: #4d4d4d;
+}
+
 @media all and (min-width: 992px) {
 .navbar .nav-item .dropdown-menu{  display:block; opacity: 0;  visibility: hidden; transition:.3s; margin-top:0;  }
-.navbar .nav-item:hover .nav-link{ color: #fff;  }
+/* .navbar .nav-item:hover .nav-link { color: #fff;  } */
 .navbar .dropdown-menu.fade-down{ top:80%; transform: rotateX(-75deg); transform-origin: 0% 0%; }
 .navbar .dropdown-menu.fade-up{ top:180%;  }
-.navbar .nav-item:hover .dropdown-menu{ transition: .3s; opacity:1; visibility:visible; top:100%; transform: rotateX(0deg);}
+.navbar .nav-item:hover .dropdown-menu { transition: .3s; opacity:1; visibility:visible; top:100%; transform: rotateX(0deg);}
 .dropdown-products::before{ content: ''; position: absolute; width: 14px; height: 0; border: 7px solid transparent; border-bottom: 7px solid #fff; top: -14px; left: 9%; -ms-transform: translateX(-50%); transform: translateX(-50%);}
-.dropdown-solutions::before{ content: ''; position: absolute; width: 14px; height: 0; border: 7px solid transparent; border-bottom: 7px solid #fff; top: -14px; left: 29%; -ms-transform: translateX(-50%); transform: translateX(-50%);}
+.dropdown-solutions::before{ content: ''; position: absolute; width: 14px; height: 0; border: 7px solid transparent; border-bottom: 7px solid #fff; top: -14px; left: 17%; -ms-transform: translateX(-50%); transform: translateX(-50%);}
 .nav-item:hover .dropdown-toggle::after{ transform: rotateZ(180deg); }
 }
 </style>
 
-<nav class="navbar navbar-expand-lg py-0 navbar-dark " style="background-color: #141d2f;">
-<div class="container">
-    <a class="navbar-brand" href="https://www.metadot.com/"><img src="./images/metadot-logo-white.png" class="brand-logo" width="451px" height="82px" alt="metadot logo"></a>
-    <button class="navbar-toggler my-1" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-between" id="navbarsExample05" >
-        <ul class="navbar-nav" >
-            <li class="nav-item dropdown" >
-                <a class=" nav-link dropdown-toggle text-white" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</a>
-                <div class="dropdown-menu dropdown-products" aria-labelledby="dropdown05" style="min-width:500px;">
-                    <div class="d-flex">
-                        <div class="d-flex flex-column">
-                            <div class="ml-2">
-                                <a class="product-item p-2" href="https://www.daskeyboard.com/">
-                                    <div class="d-flex flex-column ">
-                                        <img src="./images/dk-logo-black.png" width="49px" height="63px" alt="DasKeyboard logo"> 
-                                    </div>
-                                    <div class="d-flex flex-column ml-2">
-                                        <span class="product-title font-weight-bold">DasKeyboard</span>
-                                        <p>DasKeyboard is a company ... I write this to see if the reponsiveness is okay</p>
+<nav class="navbar navbar-expand-lg py-0" style="background-color: #141d2f;">
+    <div class="container-lg">
+        <a class="navbar-brand" href="https://www.metadot.com/"><img class="d-inline-block align-top" src="./images/metadot-logo-white.png" width="154px" height="28px" alt="metadot logo"></a>
+        <!-- <button class="navbar-toggler my-1" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button> -->
+        <div class="collapse navbar-collapse justify-content-between navbar-dark" id="navbarsExample05" >
+            <ul class="navbar-nav" >
+                <li class="nav-item dropdown" >
+                    <a class=" nav-link metadot-link dropdown-toggle text-white" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</a>
+                    <div class="dropdown-menu dropdown-products p-3 container" aria-labelledby="dropdown05" style="min-width:500px;">
+                        <div class="d-flex flex-row">
+                            <a class="product-item p-2 col-6 border-right" href="https://montastic.io">
+                                <div class="d-flex flex-column ">
+                                    <img src="./images/montastic-logo.png" width="75px" height="64px" alt="Montastic logo">                                        
                                 </div>
-                                </a>
-                            </div>
-                            <div class="ml-2">
-                                <a class="product-item p-2" href="https://bamzooka.com/">
-                                    <div class="d-flex flex-column ">
-                                        <img src="./images/bamzooka-logo.png" width="192px" height="192px" alt="metadot logo">
-                                    </div>
-                                    <div class="d-flex flex-column ml-2">
-                                        <span class="product-title font-weight-bold">Bamzooka</span>
-                                        <p>Stop the chaos. Organize team work with process checklists.</p>
+                                <div class="d-flex flex-column ml-2">
+                                    <span class="product-title font-weight-bold">Montastic Monitoring</span>
+                                    <p>Be the first to know that your website is down!</p>
                                 </div>
-                                </a>
-                            </div>
-                            <div class="ml-2">
-                                <a class="product-item p-2" href="https://montastic.io/">
-                                    <div class="d-flex flex-column ">
-                                        <img src="./images/montastic-logo.png" width="49px" height="63px" alt="DasKeyboard logo"> 
-                                    </div>
-                                    <div class="d-flex flex-column ml-2">
-                                        <span class="product-title font-weight-bold">Montastic Status</span>
-                                        <p>DasKeyboard is a company ... I write this to see if the reponsiveness is okay</p>
+                            </a>
+                            <a class="product-item p-2 col-6" href="https://montastic.io/">
+                                <div class="d-flex flex-column ">
+                                    <img src="./images/montastic-logo.png" width="49px" height="63px" alt="DasKeyboard logo"> 
                                 </div>
-                                </a>
-                            </div>
+                                <div class="d-flex flex-column ml-2">
+                                    <span class="product-title font-weight-bold">Montastic Status</span>
+                                    <p>The #1 status and incident communication tool. Keep customers and employees informed during downtime to build trust.</p>
+                                </div>
+                            </a>
                         </div>
-
-                        <div class="d-flex flex-column">
-                            <div class="mr-2">
-                                <a class="product-item p-2" href="https://mojohelpdesk.com/">
-                                    <div class="d-flex flex-column ">
-                                        <img src="./images/mojo-star.svg" width="158px" height="150px" alt="Mojo Helpdesk logo">
-                                    </div>
-                                    <div class="d-flex flex-column ml-2">
-                                        <span class="product-title font-weight-bold">Mojo Helpdesk</span>
-                                        <p>Montastic create and embed beautiful status pages with incidents and response times, to keep people informed.</p>
+                        <div class="d-flex flex-row">
+                            <a class="product-item p-2 col-6 border-right" href="https://mojohelpdesk.com/">
+                                <div class="d-flex flex-column ">
+                                    <img src="./images/mojo-star.svg" width="158px" height="150px" alt="Mojo Helpdesk logo">
                                 </div>
-                                </a>
-                            </div>
-                            <div class="mr-2">
-                                <a class="product-item p-2" href="https://montastic.io">
-                                    <div class="d-flex flex-column ">
-                                        <img src="./images/montastic-logo.png" width="75px" height="64px" alt="Montastic logo">                                        </div>
-                                    <div class="d-flex flex-column ml-2">
-                                        <span class="product-title font-weight-bold">Montastic</span>
-                                        <p>Track everything, from IT tickets to customers support. Over 2M happy users.</p>
+                                <div class="d-flex flex-column ml-2">
+                                    <span class="product-title font-weight-bold">Mojo Helpdesk</span>
+                                    <p>Delight internal and external customers with a ticket tracking & help desk system.</p>
                                 </div>
-                                </a>
-                            </div>
-                            <div class="mr-2">
-                                <a class="product-item p-2" href="https://www.daskeyboard.com/">
-                                    <div class="d-flex flex-column ">
-                                        <img src="./images/dk-logo-black.png" width="49px" height="63px" alt="DasKeyboard logo"> 
-                                    </div>
-                                    <div class="d-flex flex-column ml-2">
-                                        <span class="">DasKeyboard</span>
-                                        <p>DasKeyboard is a company ... I write this to see if the reponsiveness is okay</p>
+                            </a>
+                            <a class="product-item p-2 col-6" href="https://bamzooka.com/">
+                                <div class="d-flex flex-column ">
+                                    <img src="./images/bamzooka-logo.png" width="192px" height="192px" alt="metadot logo">
                                 </div>
-                                </a>
-                            </div>
+                                <div class="d-flex flex-column ml-2">
+                                    <span class="product-title font-weight-bold">Bamzooka</span>
+                                    <p>Organize repetitive processes into smart checklists.</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="d-flex flex-row">
+                            <a class="product-item p-2 col-6 border-right" href="https://www.daskeyboard.com/">
+                                <div class="d-flex flex-column ">
+                                    <img src="./images/dk-logo-black.png" width="49px" height="63px" alt="DasKeyboard logo"> 
+                                </div>
+                                <div class="d-flex flex-column ml-2">
+                                    <span class="product-title font-weight-bold">Das Keyboard</span>
+                                    <p>Make your fingers happy with the best keyboard in the world.</p>
+                                </div>
+                            </a>
                         </div>
                     </div>
-                 </div>
                 </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-white" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >Solutions</a>
-                <div class="dropdown-menu dropdown-solutions" aria-labelledby="dropdown05" >
-                <a class="product-item" href="#">Text</a>
-                <a class="product-item" href="#">Text</a>
-                <a class="product-item" href="#">Text</a>
-                </div>
-            </li>
-        </ul>
+                <li class="nav-item dropdown">
+                    <a class="nav-link metadot-link dropdown-toggle text-white" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >Solutions</a>
+                    <div class="dropdown-menu dropdown-solutions" aria-labelledby="dropdown05" style="min-width:250px;">
+                        <a class="dropdown-item  solutions-item" href="https://montastic.io">Website and online app monitoring</a>
+                        <a class="dropdown-item solutions-item" href="https://montastic.io">Status page for your websites and apps</a>
+                        <a class="dropdown-item solutions-item" href="https://mojohelpdesk.com/">Customer services & IT support</a>
+                        <a class="dropdown-item solutions-item" href="https://bamzooka.com/">Asset Management</a>
+                        <a class="dropdown-item solutions-item" href="https://www.daskeyboard.com/">The best keyboard in the world</a>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
     </div>
-</div>
 </nav>
-
 `;
-
-
-
-
-
-
-
-
-
-// getElementById("metadot_navbar").innerHtml("hello");
